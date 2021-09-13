daa92d8a934f0be9e56a97c71cd8c936510efd6d
dd <kdkkd@kdkd.com>
2021-09-13 10:41:49 -0500

    version 2.1.5

--- docker/run-pre-prod.sh
+++ docker/run-pre-prod.sh
@@ -3,6 +3,9 @@
 export BOT_ENV='production'
 export BOT_TAG='latest'
 export KBASE_TAG='latest'
+export DISABLE_STATE_STORAGE='true'
+export DISABLE_ANALYTICS='true'
+export CACHE_TTL='1000'
 
 export BOT_ENV=production
 export DB_HOST='vtiger-db'
@@ -10,4 +13,4 @@ export DB_NAME=becovtig
 export DB_USERNAME=becovtiguser
 export DB_PASSWORD='5g#k@&k2p'
 
-docker-compose -f docker-compose-pre-prod.yml up
+docker-compose -f docker-compose-prod-latest.yml up
